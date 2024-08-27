d97ffca1bb447bc5a8303b39b43cbf3c1f65d924
Eric-Obeng <obengeric.3958@gmail.com>
2024-08-27 15:11:49 +0000

    app config updated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,18 @@ import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { InvoiceEffects } from './store/invoice.effects';
+import { _invoiceReducer, invoiceReducer } from './store/invoice.reducer';
+import { AppState } from './store/invoice.state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideStore(),
+    provideState({ name: 'invoices', reducer: invoiceReducer }),
     provideEffects(InvoiceEffects),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
     provideHttpClient(withFetch()),
