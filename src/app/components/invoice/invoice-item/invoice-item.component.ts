ef31b68d16b239cfa4129289d886fb22ab60cfcf
Eric-Obeng <obengeric.3958@gmail.com>
2024-08-28 22:18:31 +0000

    invoice items updated to subcribe to the data from the store

--- src/app/components/invoice/invoice-item/invoice-item.component.ts
+++ src/app/components/invoice/invoice-item/invoice-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { selectAllInvoices } from '../../../store/invoice.selector';
 import { Observable } from 'rxjs';
 import { Invoice } from '../../../interfaces/invoice';
@@ -7,18 +7,22 @@ import { AppState } from '../../../store/invoice.state';
 import { HttpClient } from '@angular/common/http';
 import { loadInvoice } from '../../../store/invoice.actions';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { InvoiceStatusComponent } from "../../shared/invoice-status/invoice-status.component";
 
 @Component({
   selector: 'app-invoice-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, InvoiceStatusComponent],
   templateUrl: './invoice-item.component.html',
   styleUrl: './invoice-item.component.scss',
 })
 export class InvoiceItemComponent {
+  @Input() invoice!: Invoice;
+
   invoices$: Observable<Invoice[]> = this.store.select(selectAllInvoices);
 
-  constructor(private store: Store<AppState>, private http: HttpClient) {
+  constructor(private store: Store<AppState>, private router: Router) {
     this.invoices$.subscribe((res) => {
       console.log(res); // Log fetched invoices for debugging purposes
     });
@@ -26,5 +30,10 @@ export class InvoiceItemComponent {
 
   ngOnInit(): void {
     this.store.dispatch(loadInvoice());
+  }
+
+  viewInvoiceDeatil(invoiceId: string) {
+    console.log('navigate:', invoiceId);
+    this.router.navigate(['/invoices', invoiceId]);
   }
 }
