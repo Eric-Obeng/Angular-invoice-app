8e6138567d30ae42fc74a77da19989f8246f9b40
Eric-Obeng <obengeric.3958@gmail.com>
2024-08-28 22:17:43 +0000

    Router paths updated to show detail invoice by the invoice id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { InvoiceComponent } from './components/invoice/invoice.component';
+import { InvoiceDetailsComponent } from './components/invoice-details/invoice-details.component';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'invoices',
     component: InvoiceComponent,
+  },
+  {
+    path: 'invoices/:id',
+    component: InvoiceDetailsComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'invoices',
   },
 ];
