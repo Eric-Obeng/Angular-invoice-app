e563372ee3e584a783aa06f1bcce9c1180f376a0
Eric-Obeng <obengeric.3958@gmail.com>
2024-08-28 01:03:05 +0000

    Invoice items displayed and styled

--- src/app/components/invoice/invoice-item/invoice-item.component.ts
+++ src/app/components/invoice/invoice-item/invoice-item.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { selectAllInvoices } from '../../../store/invoice.selector';
+import { Observable } from 'rxjs';
+import { Invoice } from '../../../interfaces/invoice';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../store/invoice.state';
+import { HttpClient } from '@angular/common/http';
+import { loadInvoice } from '../../../store/invoice.actions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-invoice-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './invoice-item.component.html',
-  styleUrl: './invoice-item.component.scss'
+  styleUrl: './invoice-item.component.scss',
 })
 export class InvoiceItemComponent {
+  invoices$: Observable<Invoice[]> = this.store.select(selectAllInvoices);
 
+  constructor(private store: Store<AppState>, private http: HttpClient) {
+    this.invoices$.subscribe((res) => {
+      console.log(res); // Log fetched invoices for debugging purposes
+    });
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(loadInvoice());
+  }
 }
