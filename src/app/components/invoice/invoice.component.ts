04a24ede6dec191d3587a3266bdacb0aa8308a49
Eric-Obeng <obengeric.3958@gmail.com>
2024-08-30 16:45:41 +0000

    invoice component updated to render forms

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -3,13 +3,13 @@ import { Store } from '@ngrx/store';
 import { loadInvoice } from '../../store/invoice.actions';
 import { ButtonComponent } from '../shared/button/button.component';
 import { InvoiceFilterComponent } from './invoice-filter/invoice-filter.component';
-import { selectAllInvoices } from '../../store/invoice.selector';
+import { selectAllInvoices, selectFilteredInvoice } from '../../store/invoice.selector';
 import { Observable } from 'rxjs';
 import { Invoice, InvoiceState } from '../../interfaces/invoice';
 import { AppState } from '../../store/invoice.state';
 import { CommonModule } from '@angular/common';
 import { InvoiceItemComponent } from './invoice-item/invoice-item.component';
-import { InvoiceFormComponent } from "../invoice-form/invoice-form.component";
+import { InvoiceFormComponent } from '../invoice-form/invoice-form.component';
 
 @Component({
   selector: 'app-invoice',
@@ -19,13 +19,14 @@ import { InvoiceFormComponent } from "../invoice-form/invoice-form.component";
     InvoiceFilterComponent,
     CommonModule,
     InvoiceItemComponent,
-    InvoiceFormComponent
-],
+    InvoiceFormComponent,
+  ],
   templateUrl: './invoice.component.html',
   styleUrl: './invoice.component.scss',
 })
 export class InvoiceComponent {
-  invoices$: Observable<Invoice[]> = this.store.select(selectAllInvoices);
+  invoices$: Observable<Invoice[]> = this.store.select(selectFilteredInvoice);
+  currentStatuses: string[] = [];
 
   showForm: boolean = false;
 
@@ -45,5 +46,13 @@ export class InvoiceComponent {
 
   closeForm() {
     this.showForm = false;
+  }
+
+  handleFilterClicked() {
+    console.log('Filter Clicked');
+  }
+
+  onStatusChange(statuses: string[]) {
+    this.currentStatuses = statuses;
   }
 }
