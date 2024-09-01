09d7aba34bda00109b2078163b1811d56d0d999b
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-01 23:37:18 +0000

    effects updated

--- src/app/store/invoice.effects.ts
+++ src/app/store/invoice.effects.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { HttpClient } from '@angular/common/http';
-import { map, catchError, switchMap } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { map, catchError, switchMap, tap, mergeMap } from 'rxjs/operators';
+import { of, Observable } from 'rxjs';
 import {
   loadInvoice,
   loadInvoiceSuccess,
@@ -11,27 +11,40 @@ import {
   addInvoiceAsPending,
 } from './invoice.actions';
 import { Invoice } from '../interfaces/invoice';
+import { InvoiceService } from '../services/invoice.service';
+import { Action } from '@ngrx/store';
 
 @Injectable()
 export class InvoiceEffects {
   private apiUrl = 'assets/data.json';
 
-  constructor(private http: HttpClient, private actions$: Actions) {}
+  constructor(
+    private http: HttpClient,
+    private actions$: Actions,
+    private invoiceService: InvoiceService
+  ) {}
 
-  loadInvoices$ = createEffect(() =>
+  loadInvoices$: Observable<Action> = createEffect(() =>
     this.actions$.pipe(
       ofType(loadInvoice),
-      switchMap(() =>
-        this.http.get<Invoice[]>(this.apiUrl).pipe(
-          map((invoices) => {
-            console.log(invoices);
-            return loadInvoiceSuccess({ invoices });
-          }),
-          catchError((error) => of(loadInvoiceFailure({ error })))
-        )
-      )
+      mergeMap(() => {
+        const localStorageInvoice = localStorage.getItem('invoices');
+        if (localStorageInvoice) {
+          const invoices: Invoice[] = JSON.parse(localStorageInvoice);
+          return of(loadInvoiceSuccess({ invoices }));
+        } else {
+          return this.invoiceService.getInvoice().pipe(
+            tap((invoices: Invoice[]) => {
+              localStorage.setItem('invoices', JSON.stringify(invoices));
+            }),
+            map((invoices: Invoice[]) => {
+              localStorage.setItem('invoices', JSON.stringify(invoices));
+              return loadInvoiceSuccess({ invoices });
+            }),
+            catchError((error) => of(loadInvoiceFailure({ error })))
+          );
+        }
+      })
     )
   );
 }
-
-
